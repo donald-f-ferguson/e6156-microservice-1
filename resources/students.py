0a9526023b1062eae9cd8b80b8b6f1caa56457cd
ygq <1259698665@qq.com>
2023-09-20 23:10:52 +0800

    feat:push

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/Kami/PycharmProjects/e6156-microservice-1/resources/old-students.json"
+        "resources/old-students.json"
     def __init__(self):
         self.students = None
 
